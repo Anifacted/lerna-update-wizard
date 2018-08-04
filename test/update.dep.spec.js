896696382218d1d6e16e93b0063d8bad399bd410
Anifacted <mhoegh90@gmail.com>
2018-08-04 18:55:42 +0200

    Support adding a new package

---
+++ test/update.dep.spec.js
@@ -0,0 +1,145 @@
+const { default: runProgram } = require("./utils/runProgram");
+const generateProject = require("./utils/generateProject");
+
+describe("Update dependency", async () => {
+  let projectPath;
+
+  beforeEach(async () => {
+    projectPath = await generateProject({
+      name: "project-a",
+      packages: [
+        { name: "sub-package-a" },
+        {
+          name: "sub-package-b",
+          dependencies: { lodash: "0.1.0" },
+        },
+        { name: "sub-package-c" },
+        { name: "sub-package-d", dependencies: { lodash: "0.2.0" } },
+      ],
+    });
+  });
+
+  describe("dep already is installed in all selected packages", () => {
+    it("updates", async () => {
+      // eslint-disable-next-line
+      jest.setTimeout(100000);
+
+      await runProgram(
+        projectPath,
+        `
+        Starting update wizard for project-a
+
+        ? Select a dependency to upgrade: (Use arrow keys or type to search)
+
+        >>> input lodash
+
+        ❯ lodash (2 versions)
+
+        >>> input ENTER
+
+        ❯◯ sub-package-a
+        ◉ sub-package-b (0.1.0)
+        ◯ sub-package-c
+        ◉ sub-package-d (0.2.0)
+
+        >>> input ARROW_UP
+
+         ◯ sub-package-a
+         ◉ sub-package-b (0.1.0)
+         ◯ sub-package-c
+        ❯◉ sub-package-d (0.2.0)
+
+        >>> input ENTER
+
+        ? Select version to install: (Use arrow keys)
+
+        >>> input ARROW_UP
+        >>> input ARROW_UP
+        >>> input ARROW_UP
+
+        ❯ 0.2.2
+        0.2.1
+        0.1.0
+
+        >>> input ENTER
+
+        ? Do you want to create a new git branch for the change? (Y/n)
+
+        >>> input n
+        >>> input ENTER
+
+        ? Do you want to create a new git commit for the change? (Y/n)
+
+        >>> input n
+        >>> input ENTER
+        `,
+        { log: true }
+      );
+    });
+  });
+
+  describe("dep is new to one of the selected packages", () => {
+    it("prompts user about install-type and installs/updates", async () => {
+      // eslint-disable-next-line
+      jest.setTimeout(100000);
+
+      await runProgram(
+        projectPath,
+        `
+        Starting update wizard for project-a
+
+        ? Select a dependency to upgrade: (Use arrow keys or type to search)
+
+        >>> input lodash
+
+        ❯ lodash (2 versions)
+
+        >>> input ENTER
+
+        ❯◯ sub-package-a
+        ◉ sub-package-b (0.1.0)
+        ◯ sub-package-c
+        ◉ sub-package-d (0.2.0)
+
+        >>> input ARROW_UP
+        >>> input ARROW_UP
+        >>> input SPACE
+
+        ◯ sub-package-a
+        ◉ sub-package-b (0.1.0)
+        ❯◉ sub-package-c
+        ◉ sub-package-d (0.2.0)
+
+        >>> input ENTER
+
+        ? Select version to install: (Use arrow keys)
+
+        >>> input ARROW_UP
+        >>> input ARROW_UP
+        >>> input ARROW_UP
+
+        ❯ 0.2.2
+
+        >>> input ENTER
+
+        ❯ dependencies
+          devDependencies
+
+        >>> input ARROW_UP
+
+        ❯ devDependencies
+
+        >>> input ENTER
+
+        sub-package-c: npm install --save-dev lodash@0.2.2
+
+        >>> wait
+
+        ? Do you want to create a new git branch for the change? (Y/n)
+
+        >>> input CTRL+C
+        `
+      );
+    });
+  });
+});
