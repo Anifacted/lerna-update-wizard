6e3f5aedbc13c9d614f178ebb68f4023df13c0d8
Manol Donev <manol.donev@gmail.com>
2019-01-10 17:48:43 +0200

    Add scoped dependency support in noninteractive mode

--- src/utils/parseDependency.js
+++ src/utils/parseDependency.js
@@ -1,5 +1,17 @@
 module.exports = signature => {
-  const [name, version] = signature.split("@");
+  const firstIndex = signature.indexOf("@");
+  const lastIndex = signature.lastIndexOf("@");
+
+  let name;
+  let version;
+  if (firstIndex !== lastIndex) {
+    name = signature.substring(0, lastIndex);
+    version = signature.substring(lastIndex + 1);  
+  } else if (firstIndex > 0) {
+    [name, version] = signature.split("@");
+  } else {
+    name = signature;
+  }
 
   return { name, version };
 };
